f3e7ae54e1c64c99e6c2a03b27459be32b6217c2
peterobbin <beijingwang@gmail.com>
2017-03-13 18:10:45 -0400

    refactor to work with poScene 2.0

--- src/poSpritesheet/poSpritesheetAnimation.h
+++ src/poSpritesheet/poSpritesheetAnimation.h
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2015, Potion Design LLC
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of copyright holder nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,71 +31,70 @@
 #pragma once
 
 #include "poSpritesheet.h"
-#include "boost/signals2.hpp"
 
-namespace po {
-	
+namespace po
+{
+
 	class Spritesheet;
 	typedef std::shared_ptr<Spritesheet> SpritesheetRef;
-	
+
 	class SpritesheetAnimation;
 	typedef std::shared_ptr<SpritesheetAnimation> SpritesheetAnimationRef;
-	
-	class SpritesheetAnimation
-	: public std::enable_shared_from_this<SpritesheetAnimation>
-	{
-	public:
-		//	Spritesheet to animate and frame rate
-		static SpritesheetAnimationRef create(SpritesheetRef spritesheet, float fps = 12.f);
-		
-		~SpritesheetAnimation();
-		
-		//	Playing complete signal, when not looped
-		typedef ci::signals::Signal<void(SpritesheetAnimationRef)> SignalSpritesheetPlayComplete;
-		
-		virtual void update();
-		virtual void draw();
-		
-        void play();
-		void pause() { mIsPlaying = false; }
-		void stop();
-		void setIsLoopingEnabled(bool isLooping) { mIsLooping = isLooping; }
-		void setFrameRate(float frameRate);
-		void setIsReverse(bool reverse);
-		
-		SignalSpritesheetPlayComplete &getSignalPlayingComplete() { return mPlayCompleteSignal; }
-		int getCurrentFrame() { return mCurrentFrame; }
-		bool getIsPlaying() { return mIsPlaying; }
-		bool getIsReverse() { return mIsReverse; }
 
-		
-	protected:
-		SpritesheetAnimation();
-		
-		void setup(SpritesheetRef spritesheet, float fps);
-		
-	private:
-		SpritesheetRef mSpritesheet;
-		
-		//	Keep track of frames
-		int mCurrentFrame;
-		int mLastFrame;
-		
-		//	Keep track of state
-		bool mIsPlaying;
-		bool mIsLooping;
-		bool mIsReverse;
-		
-		float mFrameRate;
-		float mFPS;
-		float mCurrentTime;
-		float mPreviousTime;
-		
-		SignalSpritesheetPlayComplete mPlayCompleteSignal;
-		
-		//	Proceed to next frame
-		void nextFrame();
-		
+	class SpritesheetAnimation
+		: public std::enable_shared_from_this<SpritesheetAnimation>
+	{
+		public:
+			//	Spritesheet to animate and frame rate
+			static SpritesheetAnimationRef create( SpritesheetRef spritesheet, float fps = 12.f );
+
+			~SpritesheetAnimation();
+
+			//	Playing complete signal, when not looped
+			typedef ci::signals::Signal<void( SpritesheetAnimationRef )> SignalSpritesheetPlayComplete;
+
+			virtual void update();
+			virtual void draw();
+
+			void play();
+			void pause() { mIsPlaying = false; }
+			void stop();
+			void setIsLoopingEnabled( bool isLooping ) { mIsLooping = isLooping; }
+			void setFrameRate( float frameRate );
+			void setIsReverse( bool reverse );
+
+			SignalSpritesheetPlayComplete& getSignalPlayingComplete() { return mPlayCompleteSignal; }
+			int getCurrentFrame() { return mCurrentFrame; }
+			bool getIsPlaying() { return mIsPlaying; }
+
+
+		protected:
+			SpritesheetAnimation();
+
+			void setup( SpritesheetRef spritesheet, float fps );
+
+		private:
+			SpritesheetRef mSpritesheet;
+
+			//	Keep track of frames
+			int mCurrentFrame;
+			int mLastFrame;
+
+			//	Keep track of state
+			bool mIsPlaying;
+			bool mIsLooping;
+			bool mIsReverse;
+
+			float mFrameRate;
+			float mFPS;
+			float mCurrentTime;
+			float mPreviousTime;
+
+			SignalSpritesheetPlayComplete mPlayCompleteSignal;
+
+			//	Proceed to next frame
+			void nextFrame();
+
 	};
-	
+
 }
